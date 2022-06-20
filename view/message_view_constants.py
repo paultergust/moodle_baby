42d195505aa6401d5ce839117b7fc9eb76346510
MarlonCorreia <malilocsimas26@gmail.com>
2022-06-20 16:42:46 -0300

    Add msg, parent and teacher view constants

---
+++ view/message_view_constants.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+class CREATE(Enum):
+    ACTION = ''
+    SUCCESS = ''
+    FAILED = ''
+
+class UPDATE(Enum):
+    ACTION = ''
+    SUCCESS = ''
+    FAILED = ''
+
+class DELETE(Enum):
+    ACTION = ''
+    SUCCESS = ''
+    FAILED  = ''
+
+class RETRIEVE(Enum):
+    ACTION = ''
+    SUCCESS = ''
+    FAILED  = ''
+
+class LIST(Enum):
+    ACTION = ''
+    SUCCESS = ''
+    FAILED  = ''
