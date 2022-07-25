317a071e51173bef70f804e10a9f159d95d60ebe
marloncorreia <malilocsimas26@gmail.com>
2022-07-25 01:45:27 -0300

    fix: use msg controller

---
+++ controller/message_controller.py
@@ -0,0 +1,22 @@
+from model import Message
+from daos.messageDAO import MessageDAO
+
+
+class MessageController():
+
+    def __init__(self) -> None:
+        self.__dao = MessageDAO()
+
+    def list_messages(self, obj):
+        msg_list = {}
+        for msg in self.__dao.get_all():
+            if msg.sender == obj or msg.receiver == obj:
+                msg_list[msg.id] = msg
+        
+        return msg_list
+    
+    def get(self, id):
+        return self.__dao.get(id)
+    
+    def delete(self, id):
+        self.__dao.delete(id)
--- controller/parent_controller.py
+++ controller/parent_controller.py
@@ -1,15 +1,15 @@
 from model import Parent
 from daos.parentDAO import ParentDAO
-from daos.messageDAO import MessageDAO
 from view import parent_view_constants as parent_constants
 from view import message_view_constants as msg_constants
 from controller.main_controller import MainController
+from controller.message_controller import MessageController
 
 class ParentController(MainController):
 
     def __init__(self) -> None:
         self.__dao = ParentDAO()
-        self.__msg_dao = MessageDAO()
+        self.__msg_controller = MessageController()
     
     def list(self):
         parent_list = self.__dao.get_all()
@@ -22,20 +22,17 @@ class ParentController(MainController):
 
 
     def list_messages(self, parent):
-        msg_list = {}
-        for msg in self.__msg_dao.get_all().values():
-            if msg.sender == parent or msg.receiver == parent:
-                msg_list[msg.id] = msg
+        msg_list = self.__msg_controller.list_messages(parent)
         
 
     def retrieve_message(self, parent, msg_id):
-        msg = self.__msg_dao.get(msg_id)
+        msg = self.__msg_controller.get(msg_id)
         if msg:
             pass
 
     def delete_message(self, parent, msg_id):
-        if self.__msg_dao.get(msg_id):
-            self.__msg_dao.delete(msg_id)    
+        if self.__msg_controller.get(msg_id):
+            self.__msg_controller.delete(msg_id)    
             msg_list = self.list_messages(parent)
 
 
