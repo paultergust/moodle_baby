953fee1f1bdd87a07c821c523fa413e4cce10754
marloncorreia <malilocsimas26@gmail.com>
2022-07-25 02:07:19 -0300

    feat: msg controller create

--- controller/message_controller.py
+++ controller/message_controller.py
@@ -1,10 +1,12 @@
 from model import Message
 from daos.messageDAO import MessageDAO
+from datetime import datetime
 
 
 class MessageController():
 
     def __init__(self) -> None:
+        self.__model = Message
         self.__dao = MessageDAO()
 
     def list_messages(self, obj):
@@ -14,6 +16,10 @@ class MessageController():
                 msg_list[msg.id] = msg
         
         return msg_list
+    
+    def create(self, sender, receiver, subject, body):
+        new_msg  = self.__model(sender, receiver, subject, body, datetime.now())
+        self.__dao.add(new_msg.id, new_msg)
     
     def get(self, id):
         return self.__dao.get(id)
